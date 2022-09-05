d7c47befb8902f4fd83e3fdb3943e25b5cca1035
Noor696 <eng.nooralhateeb@gmail.com>
2022-09-05 07:07:23 +0300

    done

--- README.md
+++ README.md
@@ -6,6 +6,45 @@ This repository has starter code to begin work on creating your own online books
 
 Books are life-changing. They have the power to enlighten, educate, entertain, heal, and help us grow. Build out this code base to create an web app to track what books have impacted you, and what's recommended to read next.
 
-## Use this template
+---------------
+# "Can of Books" App
 
-Clone this repo to your own account with the green "Use this template" button. Then, add any collaborators. Now you are ready to start adding features! Deploy your site to let the world know which books have had the greatest impact on you, and what's recommended. Enjoy!
+
+**Author**: Noor Alkhateeb
+**Version**: 1.0.0 
+
+## Overview
+This repository has starter code to begin work on creating your own online bookshelf.
+
+**What can books do?**
+
+Books are life-changing. They have the power to enlighten, educate, entertain, heal, and help us grow. Build out this code base to create an web app to track what books have impacted you, and what's recommended to read next.
+
+## Getting Started
+1. Set up your repositories
+2. Storage: As a user, I'd like to view the list of books, so that whenever I visit the page, I can see the a full list of best books.
+  - 
+3. Createt the front end
+4. send request to the server
+
+## Architecture
+
+
+## Change Log
+
+04-09-2022 11:59pm - 
+
+## Estimates
+
+Name of feature: server and mongo DB
+
+Estimate of time needed to complete: 5 hrs
+
+Start time: 5:pm
+
+Finish time: --pm
+
+Actual time needed to complete: -- hrs
+
+
+## Credit and Collaborations
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from "axios";
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,22 +12,59 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+
+  componentDidMount = () => {
+    axios
+      .get("http://localhost:3005/books")
+      .then((result) => {
+        this.setState({
+          books: result.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
 
     /* TODO: render all the books in a Carousel */
 
     return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+     
+      <div>
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+
+          <div>
+          
+      <Carousel fade>
+      {this.state.books.map((item) => {
+        return(
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="slide"
+        />
+        <Carousel.Caption>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+          <p>{item.status}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      )})}
+      </Carousel>
+      </div>
+        //<h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+
         ) : (
           <h3>No Books Found :(</h3>
         )}
-      </>
-    )
-  }
-}
+    
+      </div>
+    ); //return
+  } //render
+} //class
+
 
 export default BestBooks;
